d90ffb8eeaad0e62da45a308a35b1f4c79557124
Ahmadhaiwala <haiwalaahmad7@gmail.com>
2026-09-09 22:51:54 +0530

    initialization

---
+++ backend/app/models/users.py
@@ -0,0 +1,33 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    username:mapped_column(
+        varchar
+    )
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
