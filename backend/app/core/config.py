7cdaf2c6ff40d95e891b72e66c8f7bdfc94bbcb0
Ahmadhaiwala <haiwalaahmad7@gmail.com>
2026-09-12 01:23:00 +0530

    db config improvement

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     API_V1_PREFIX: str = "/api/v1"
     
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+    DATABASE_URL: str = "postgresql+psycopg://postgres:Ahmad4444@localhost:5432/task_manager"
     
     class Config:
         case_sensitive = True
